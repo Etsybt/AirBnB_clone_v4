a86c92e7ef8a469acca6109cfd966ef1936b7e79
Etsybt <hmidouchnouhaila2@gmail.com>
2024-02-11 16:54:48 +0000

    update

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,48 +1,41 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-import uuid
-from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.user import User
-from os import environ
+"""
+Flask App
+"""
 from flask import Flask, render_template
+from models import storage
+import uuid
+
+# Flask setup
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
+app.url_map.strict_slashes = False
+port = 5000
+host = '0.0.0.0'
 
 
+# Handle teardown of the database connection
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown_db(exception):
+    """
+    Closes the SQLAlchemy session after each request
+    """
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/1-hbnb/')
 def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    users = storage.all(User).values()
-    users = sorted(users, key=lambda k: k.name)
-
+    """
+    Renders a custom template with states, cities, and amenities
+    """
+    state_objs = storage.all('State').values()
+    states = dict([(state.name, state) for state in state_objs])
+    amenities = storage.all('Amenity').values()
+    places = storage.all('Place').values()
+    users = dict([(user.id, f"{user.first_name} {user.last_name}")
+                  for user in storage.all('User').values()])
     cache_id = uuid.uuid4()
-
     return render_template('1-hbnb.html',
-                           states=st_ct,
+                           states=states,
                            amenities=amenities,
                            places=places,
                            users=users,
@@ -50,5 +43,7 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    """
+    Main function to run the Flask app
+    """
+    app.run(host=host, port=port)
